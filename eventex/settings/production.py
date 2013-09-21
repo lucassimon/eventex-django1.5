7afefb77643ff5f53cfe2bf57754756b9f03bc6c
Lucas Simon Rodrigues Magalhaes <lucassrod@gmail.com>
2013-09-21 18:23:35 -0300

    Insere configuracoes da amazon no settings

--- eventex/settings/production.py
+++ eventex/settings/production.py
@@ -1,4 +1,5 @@
 from base import *
+import os
 
 ########## DATABASE CONFIGURATION
 DATABASES = {
@@ -12,3 +13,22 @@ DATABASES = {
     }
 }
 ########## END DATABASE CONFIGURATION
+
+
+##########  AMAZON S3 CONFIGURATION
+
+# 1 - Create the variables envoirement
+# AWS_STORAGE_BUCKET_NAME='test'
+# 2 - Export the varables
+# export AWS_STORAGE_BUCKET_NAME
+# 3 - Get the secret and access key in https://console.aws.amazon.com/iam/home?#security_credential
+
+if not DEBUG:
+    COMPRESS_ENABLED = not DEBUG
+    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+    STATICFILES_STORAGE = COMPRESS_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    STATIC_URL = COMPRESS_URL = S3_URL
+########## END AMAZON S3 CONFIGURATION
