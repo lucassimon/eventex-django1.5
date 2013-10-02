dd007d12d42a151719fdb67e11908d19b28ed285
Lucas Simon <lucassrod@gmail.com>
2013-10-02 10:25:58 -0300

    Retira comentarios do SETTINGS DATADABASE

--- eventex/settings/production.py
+++ eventex/settings/production.py
@@ -4,12 +4,12 @@ import os
 ########## DATABASE CONFIGURATION
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': 'postgres',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 ########## END DATABASE CONFIGURATION
@@ -38,6 +38,4 @@ if not DEBUG:
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = [
-    '.eventexlucassimon.com.br'
-]
+ALLOWED_HOSTS = '*'
