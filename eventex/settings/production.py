9cc54c479566bafa0739e616aadfdc760c8a74b6
Lucas Simon Rodrigues Magalhaes <lucassrod@gmail.com>
2013-10-02 21:59:43 -0300

    Organiza as configuracoes do compress e AWS

--- eventex/settings/production.py
+++ eventex/settings/production.py
@@ -27,18 +27,32 @@ INSTALLED_APPS += (
 # 3 - Get the secret and access key in https://console.aws.amazon.com/iam/home?#security_credential
 
 if not DEBUG:
-    COMPRESS_ENABLED = not DEBUG
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-    STATICFILES_STORAGE = COMPRESS_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    STATICFILES_STORAGE = 'storage.CachedS3BotoStorage'
     S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-    STATIC_URL = COMPRESS_URL = S3_URL
+    AWS_IS_GZIPPED = True
+    AWS_QUERYSTRING_EXPIRE = 7200
+    AWS_S3_SECURE_URLS = True
+    STATIC_URL = S3_URL
+########## END AMAZON S3 CONFIGURATION
+
+
+##########  COMPRESS CONFIGURATION
+if not DEBUG:
+    COMPRESS_ENABLED = not DEBUG
+    COMPRESS_OFFLINE = True
+    COMPRESS_ROOT = STATIC_ROOT
+    COMPRESS_STORAGE = 'storage.CachedS3BotoStorage'
+
+    COMPRESS_URL = S3_URL
     COMPRESS_PRECOMPILERS = (
         ('text/coffeescript', 'coffee --compile --stdio'),
         ('text/less', 'lessc {infile} > {outfile}')
     )
-########## END AMAZON S3 CONFIGURATION
+
+##########  COMPRESS CONFIGURATION
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
